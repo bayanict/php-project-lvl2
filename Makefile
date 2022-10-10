4f9bfed68378afc89a066221944708467404a5d8
bayanict <aidar00776@gmail.com>
2022-10-10 12:32:20 +0300

    Part of task 3

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 install:
 	composer install
+
+validate:
+	composer validate
--- src/Differ.php
+++ src/Differ.php
@@ -2,3 +2,13 @@
 
 namespace Differ\Differ;
 
+function genDiff($firstFile, $secondFile)
+{
+    $firstFileContent = file_get_contents($firstFile);
+    $secondFileContent = file_get_contents($secondFile);
+
+    $firstFileDecoded = json_decode($firstFileContent, true);
+    $secondFileDecoded = json_decode($secondFileContent, true);
+
+    return [$firstFileDecoded["proxy"], $secondFileDecoded];
+}
